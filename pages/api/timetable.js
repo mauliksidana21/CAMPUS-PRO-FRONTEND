24c2d578ab1c0e27f75cc54d6c1faaa9c73627f3
mauliksidana21 <mauliksidana@gmail.com>
2026-03-18 15:17:10 +0530

    Update timetable.js

--- pages/api/timetable.js
+++ pages/api/timetable.js
@@ -1,9 +1,11 @@
 export default async function handler(req, res) {
   const token = req.headers['x-token']
   try {
-    
+    const r = await fetch('https://campus-pro-backend-1.onrender.com/timetable', {
       headers: { 'X-CSRF-Token': 'undefined', 'token': token }
     })
     res.status(200).json(await r.json())
-const r = await fetch('https://campus-pro-backend-1.onrender.com/timetable', {  } catch (e) { res.status(500).json({ error: e.message }) }
+  } catch (e) {
+    res.status(500).json({ error: e.message })
+  }
 }
